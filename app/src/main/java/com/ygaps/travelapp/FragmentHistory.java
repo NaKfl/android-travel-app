fac4659b4e550e6c464e72d641cb1331b4370ded
NaKfl <o0liebeo0o@gmail.com>
2019-12-28 18:45:03 +0700

    Fix lỗi

--- app/src/main/java/com/ygaps/travelapp/FragmentHistory.java
+++ app/src/main/java/com/ygaps/travelapp/FragmentHistory.java
@@ -13,11 +13,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
